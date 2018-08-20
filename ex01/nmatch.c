a6069176eac71bc24a51ced202495cdc2630be8a
msyukyu <damien_ab@hotmail.com>
2018-08-20 22:52:18 +0200

    nmatch

--- ex01/nmatch.c
+++ ex01/nmatch.c
@@ -10,27 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int		nmatch(char *s1, char *s2)
 {
 	int	count;
 
 	if (*s2 == '\0')
-		return ((*s1 == '\0') ? 1 : 0);
-	if (*s2 == '*')
-	{
-		count = 0;
-		while (*s1 != '\0')
-		{
-			count += nmatch(s1, (s2 + 1));
-			s1++;
-		}
-		count += nmatch(s1, s2 + 1);
-		return (count);
-	}
+		return (*s1 == '\0');
 	if (*s1 == '\0')
-		return (0);
+		return ((*s2 == '*') ? nmatch(s1, (s2 + 1)) : 0);	
+	if (*s2 == '*')
+		return (nmatch(s1, (s2 + 1)) + nmatch((s1 + 1), s2));
 	if (*s1 == *s2)
 		return (nmatch((s1 + 1), (s2 + 1)));
-	else
-		return (0);
+	return (0);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d", nmatch(argv[1], argv[2]));
+	}
+	return (0);
 }
